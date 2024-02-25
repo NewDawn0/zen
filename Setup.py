0bbf24142c4d1e5a2dfeaa168d9d68ca4364a0e9
NewDawn0 <aci.v.i.iv@gmail.com>
2024-02-25 20:32:07 +0100

    Add userConfig in .config

--- Setup.py
+++ Setup.py
@@ -1,7 +1,22 @@
+import os
+from shutil import copyfile
+
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIREMENTS_FILE = "requirements.txt"
+
+def copy_user_conf():
+    config_dir = os.path.expanduser("~/.config/zen")
+    if not os.path.exists(config_dir):
+        os.makedirs(config_dir)
+    main_py_dst = os.path.join(config_dir, "__init__.py")
+    if not os.path.exists(main_py_dst):
+        copyfile("gen/__init__.py", main_py_dst)
+
+
+copy_user_conf()
+
+REQUIREMENTS_FILE = "zen/requirements.txt"
 REQUIRED = None
 with open(REQUIREMENTS_FILE, "r") as f:
     REQUIRED = [line.strip() for line in f]
@@ -16,4 +31,5 @@ setup(
             "zen = zen.main:main",
         ]
     },
+    data_files=[(os.path.expanduser("~/.config/zen"), ["gen/__init__.py"])],
 )
--- zen/utils/globals/config.py
+++ zen/utils/globals/config.py
@@ -1,4 +1,16 @@
-# Import and load user configuration from the main function in userConfig module
-from userConfig.main import main
+# Import and load user config from the main function in userConfig module
+import os
+import sys
 
-CONFIG = main()
+main_py_path = os.path.expanduser("~/.config/zen/__init__.py")
+if not os.path.exists(main_py_path):
+    exit("__init__.py not found in ~/.config/zen/")
+
+# Push the user config
+sys.path.insert(0, os.path.dirname(main_py_path))
+import __init__ as user
+
+CONFIG = user.main()
+
+# Pop the user config
+sys.path.pop(0)
