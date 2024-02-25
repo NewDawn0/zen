7cf56fd06ff53f4761add28b22dcfc1d9874ffc1
NewDawn0 <aci.v.i.iv@gmail.com>
2024-02-26 00:16:36 +0100

    Update cleanupfile

--- clean.sh
+++ clean.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
-ZEN_FILES=(".mypy_cache" ".pytest_cache" "__pycache__" "api/__pycache__/" "components/__pycache__/" "tests/__pycache__/" "userConfig/__pycache__/" "utils/__pycache__/" "utils/globals/__pycache__/" "build")
-CLEAN_FILES=("build" "zen.egg-info")
+ZEN_FILES=(
+    "zen/.mypy_cache"
+    "zen/.pytest_cache"
+    "zen/__pycache__"
+    "zen/api/__pycache__/"
+    "zen/components/__pycache__/"
+    "zen/tests/__pycache__/"
+    "zen/userConfig/__pycache__/"
+    "zen/utils/__pycache__/"
+    "zen/utils/globals/__pycache__/"
+    "zen/build"
+    "build"
+    "zen.egg-info"
+    "$HOME/.config/zen/__pycache__/"
+)
 for file in ${ZEN_FILES[@]}; do
-    rm -rf "zen/$file"
-done
-
-for file in ${CLEAN_FILES[@]}; do
     rm -rf "$file"
 done
